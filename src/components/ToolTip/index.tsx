28bb8064d5987e637892b834b2f77eef87637de4
higoraln <Higor Allan>
2022-11-06 23:24:28 -0300

    change color of the tooltip

--- src/components/ToolTip/index.tsx
+++ src/components/ToolTip/index.tsx
@@ -13,7 +13,7 @@ export function Tooltip({ children, content, duration = 300 }: Props) {
       <TooltipRadix.Root>
         <TooltipRadix.Trigger>{children}</TooltipRadix.Trigger>
         <TooltipRadix.Portal>
-          <TooltipRadix.Content className="bg-gray-800 text-white px-6 py-2 rounded-md">
+          <TooltipRadix.Content className="bg-gray-800 dark:bg-style-g-2 text-white px-6 py-2 rounded-md">
             {content}
             <TooltipRadix.Arrow />
           </TooltipRadix.Content>
