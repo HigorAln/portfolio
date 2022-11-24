b3980e93b0d5de4ff34e63268096fadc14d2c30d
Higor Allan <higor.allan21@gmail.com>
2022-11-24 08:39:49 -0300

    update

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -11,61 +11,103 @@ export function Projects() {
   if (error) return null;
 
   return (
-    <m.div
-      className="z-10 flex flex-col items-center w-full px-5 md:px-28"
-      id="studies"
-    >
+    <m.div className="z-10 flex flex-col items-center w-full px-5" id="studies">
       <div className="max-w-7xl w-full">
-        <m.p className="text-5xl md:text-4xl font-poiret text-gray-900 dark:text-style-g-ed tracking-widest pt-20">
+        <m.p
+          initial={{ opacity: 0, y: 10 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1 }}
+          viewport={{ once: true }}
+          className="text-2xl md:text-4xl font-poiret text-gray-900 dark:text-style-g-ed tracking-widest pt-20"
+        >
           a brief history
         </m.p>
-        <m.p className="text-5xl md:text-2xl mt-3 font-poiret mb-8 md:mb-24 text-gray-900 dark:text-style-g-ed tracking-widest">
+        <m.p
+          initial={{ opacity: 0, y: 10 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1 }}
+          viewport={{ once: true }}
+          className="text-2xl md:text-2xl mt-3 font-poiret mb-8 md:mb-24 text-gray-900 dark:text-style-g-ed tracking-widest"
+        >
           my story is brief but my evolution is constant
         </m.p>
 
         <div className="w-full flex flex-col gap-2">
-          <span className="grid grid-cols-3">
+          <span className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+            <span className="hidden md:flex"></span>
             <span></span>
-            <span></span>
-            <div className="flex justify-between bg-slate-400 p-3 rounded-lg mr-20 -ml-20">
+            {/* PRIMEIRO */}
+            <m.div
+              initial={{ opacity: 0, x: 20 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ delay: 1 }}
+              viewport={{ once: true }}
+              className="flex justify-between bg-slate-400 dark:bg-slate-700 shadow-xl shadow-gray-300 dark:shadow-gray-900 p-3 rounded-lg md:mr-20 md:-ml-20"
+            >
               <span>
-                <p className="font-poiret text-3xl">Patriani</p>
-                <p className="font-poiret text-lg">Web Developer</p>
+                <p className="font-poiret text-3xl dark:text-style-w">
+                  Patriani
+                </p>
+                <p className="font-poiret text-lg dark:text-style-w">
+                  Web Developer
+                </p>
               </span>
               <span className="flex-1"></span>
 
               <span className="h-full flex items-center">
-                <p className="text-3xl font-poiret">08-22</p>
+                <p className="text-3xl font-poiret dark:text-style-w">08-22</p>
               </span>
-            </div>
+            </m.div>
           </span>
-          <span className="grid grid-cols-3">
+          <span className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
             <span></span>
-            <div className="flex justify-between bg-slate-400 p-3 rounded-lg">
-              <span>
-                <p className="font-poiret text-3xl">Workfy</p>
-                <p className="font-poiret text-lg">Web Developer</p>
-              </span>
+            {/* Segundo */}
+            <m.div
+              initial={{ opacity: 0, x: 20 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ delay: 1.4 }}
+              viewport={{ once: true }}
+              className="flex justify-between bg-slate-400 dark:bg-slate-700 shadow-xl shadow-gray-300 dark:shadow-gray-900 p-3 rounded-lg"
+            >
+              <div>
+                <p className="font-poiret text-3xl dark:text-style-w">Workfy</p>
+                <p className="font-poiret text-lg dark:text-style-w">
+                  Web Developer
+                </p>
+              </div>
               <span className="flex-1"></span>
 
               <span className="h-full flex items-center">
-                <p className="text-3xl font-poiret">02-22</p>
+                <p className="text-3xl font-poiret dark:text-style-w">02-22</p>
               </span>
-            </div>
+            </m.div>
             <span></span>
           </span>
-          <span className="grid grid-cols-3">
-            <div className="flex justify-between bg-slate-400 p-3 rounded-lg ml-20 -mr-20">
+          <span className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+            {/* terceiro */}
+            <m.div
+              initial={{ opacity: 0, x: 20 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ delay: 1.7 }}
+              viewport={{ once: true }}
+              className="flex justify-between bg-slate-400 dark:bg-slate-700 shadow-xl shadow-gray-300 dark:shadow-gray-900 p-3 rounded-lg md:ml-20 md:-mr-20"
+            >
               <span>
-                <p className="font-poiret text-3xl">Freelancer</p>
-                <p className="font-poiret text-lg">Web Developer</p>
+                <p className="font-poiret text-3xl text-style-background dark:text-style-w">
+                  Freelancer
+                </p>
+                <p className="font-poiret text-lg text-style-background dark:text-style-w">
+                  Web Developer
+                </p>
               </span>
               <span className="flex-1"></span>
 
               <span className="h-full flex items-center">
-                <p className="text-3xl font-poiret">11-21</p>
+                <p className="text-3xl font-poiret text-style-background dark:text-style-w">
+                  11-21
+                </p>
               </span>
-            </div>
+            </m.div>
             <span></span>
             <span></span>
           </span>
